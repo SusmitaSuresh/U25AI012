2b3c2e56e316fabbd530c9326fb33aaf9a3c728b
SusmitaSuresh <susmitacs1209@gmail.com>
2025-09-06 10:43:45 +0530

    Delete lab1_pr13.c

--- lab1_pr13.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int num;
-    printf("enter a random number: ");
-    scanf("%d", &num);
-    num%2==0 ? printf("the number %d is even", num) : printf("the number %d is odd", num);
-    return 0;
-}
